085702a0b70e487e0721d83d8eaab62f574f1f96
Xinyu Sui <suix2@uw.edu>
2016-09-12 23:05:19 +0800

    evaluator 1

--- Evaluator.hs
+++ Evaluator.hs
@@ -33,7 +33,12 @@ primitives = [("+", nbop (+)),
               ("/", nbop div),
               ("mod", nbop mod),
               ("quotient", nbop quot),
-              ("remainder", nbop rem)]
+              ("remainder", nbop rem),
+              ("list?", uop listp),
+              ("symbol?", uop symbolp),
+              ("string?", uop stringp),
+              ("number?", uop numberp),
+              ("bool?", uop boolp)]
 
 nbop :: (Integer -> Integer -> Integer) -> [LispVal] ->LispVal
 nbop op args = Number $ foldl1 op $ map unpackNum args
@@ -42,6 +47,28 @@ unpackNum :: LispVal -> Integer
 unpackNum (Number n) = n
 unpackNum (List [n]) = unpackNum n
 unpackNum _          = 0
+
+uop :: (LispVal -> LispVal) -> [LispVal] -> LispVal
+uop f [v] = f v
+
+listp, symbolp, stringp, numberp, boolp :: LispVal -> LispVal
+listp (List _)       = Bool True
+listp (DottedList _) = Bool True 
+listp -              = Bool False
+symbolp (Atom _)     = Bool True
+symbolp _            = Bool False
+stringp (String _)   = Bool True
+stringp _            = Bool False
+numberp (Number _)   = Bool True
+numberp _            = Bool False
+boolp (Bool _)       = Bool True
+boolp _              = Bool False
+
+symbol2string, string2symbol :: LispVal -> LispVal
+symbol2string (Atom s)   = String s
+symbol2string _          = String ""
+string2symbol (String s) = Atom s
+string2symbol _          = Atom ""
 
 
 
